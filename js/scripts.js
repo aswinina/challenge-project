ffd95da3941fae1fd93cd34cb17bbd8a4de7af78
aswinina <116088497+aswinina@users.noreply.github.com>
2023-02-10 15:58:59 +0100

    Added the slider container

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 hamburger = document.querySelector(".hamburger");
 
 // add a function that runs when the hamburger is
@@ -8,4 +10,20 @@ hamburger.addEventListener("click", function() {
     // method to toggle the active class
     navbar = document.querySelector(".nav-bar");
     navbar.classList.toggle("active");
+});
+
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+  },
+    loop: true,
+    pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
 });
