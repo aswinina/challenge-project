ed880f7193111aa10ce88999680703ac5ace63ba
aswinina <116088497+aswinina@users.noreply.github.com>
2023-02-10 09:03:53 +0100

    cgange onclick function

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,11 @@
 hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function() {
+
+// add a function that runs when the hamburger is
+// clicked
+hamburger.addEventListener("click", function() {
+
+    // query the navbar and use its classList.toggle 
+    // method to toggle the active class
     navbar = document.querySelector(".nav-bar");
     navbar.classList.toggle("active");
-}
+});
