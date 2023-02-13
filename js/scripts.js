44e143619bb48cbb3da48c82e6a497785531688c
aswinina <116088497+aswinina@users.noreply.github.com>
2023-02-13 21:55:29 +0100

    fixed the slider code

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,31 @@ hamburger.addEventListener("click", function() {
     navbar.classList.toggle("active");
 });
 
-const swiper = new Swiper('.swiper', {
-  autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-  },
-    loop: true,
-    pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+let slideIndex = 1;
+showSlides(slideIndex);
 
-});
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+}
